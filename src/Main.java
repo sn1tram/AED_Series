a41bff199c81028604caa4828d5f5fae07d89265
Pedro Santos <pedro.m.paixao.santos@gmail.com>
2016-10-23 11:55:52 +0100

    A unica cena que falta sao os null quando deixa de ter linha para ler.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import Problem.GenericHeap;
 import Problem.GetFile;
 
 import java.awt.print.Printable;
 import java.text.ParseException;
+import java.util.Comparator;
 
 /**
  * Created by Pedro on 15/10/2016.
@@ -20,25 +22,57 @@ public class Main {
 		System.out.println(s1.compareTo("")); //1
 		//System.out.println(s1.compareTo(null));
 		System.out.println(s1.equals(null));*/
+		//GetFile getFile = new GetFile(n, s1, s2, s3);
 
+		Integer[] maxHeap = {18, 15, 16, 10, 9, 12, 14, 3, 4, 5, 2 };
+		int [] array = {1, 2, 3, 4, 7, 8, 9, 10, 14, 16};
 
-		GetFile getFile = new GetFile(n, s1, s2, s3);
-		/*String[] ipv4 = new String[]{"192.168.1.6", "123.123.123.122"};
-		String Str = new String("Welcome-to-Tutorialspoint.com");
-		System.out.println("Return Value :" );
-		String[] x = ipv4[0].split("\\.");
-
-		int aux = Integer.parseInt(x[0]);
-		System.out.println(aux);
-
-		/*for (String retval: ipv4[0].split("\\.")) {
-			int x = Integer.parseInt(retval);
-			System.out.println(x);
-		}*/
+		buildMaxHeap(array, array.length);
+		show(array);
 	}
 
+	public static void changeValueInMaxHeap(int[] v, int count, int ix, int newValue){
 
-	public static void sortIPv4Addresses(String[] v, int l, int r){
+	}
+
+	public static void show(int[] t) {
+		for (int i = 0; i < t.length; i++)
+			System.out.print(t[i] + " ");
+		System.out.println();
+	}
+
+	public static void buildMaxHeap(int[] h, int n) {
+		for (int i = n / 2 - 1; i >= 0; i--)
+			maxHeapify(h, i, n);
+	}
+
+	/*public static int parent(int i) { return (i - 1) / 2; } */
+
+	public static int left(int i) { return 2 * i + 1; }
+
+	public static int right(int i) { return 2 * i + 2; }
+
+	public static void maxHeapify(int[] h, int i, int n) {
+		int l = left(i);
+		int r = right(i);
+		int largest;
+		if (l < n && h[l] > h[i])
+			largest = l;
+		else largest = i;
+		if (r < n && h[r] > h[largest])
+			largest = r;
+		if (largest != i) {
+			exchange(h, i, largest);
+			maxHeapify(h, largest, n);
+		}
+	}
+
+	private static void exchange(int[] h, int i, int j) {
+		int aux = h[i];
+		h[i] = h[j];
+		h[j] = aux;
+	}
+	/*public static void sortIPv4Addresses(String[] v, int l, int r){
 		int idx, i = l+1;
 		int a1, a2;
 		String[] aux1 = new String[(r-l)+1];
@@ -50,5 +84,5 @@ public class Main {
 			a1 = Integer.parseInt(aux1[0]);
 			a2 = Integer.parseInt(aux2[0]);
 		}
-	}
+	}*/
 }
--- src/series/serie1/Arrays.java
+++ src/series/serie1/Arrays.java
@@ -8,7 +8,6 @@ public class Arrays {
     //EX1
     public static int greatestAfterRotate(int[] v, int l, int r){
         if(l > r) return -1;
-        //int bigger;
         while (l<r){
             if(v[l]>v[r]) --r;
             if(v[l]<v[r]) ++l;
@@ -21,11 +20,8 @@ public class Arrays {
         if(l1-r1 != l2-r2) return false;
         int count = 0;
         for(int i = l1; i<=r1; i++) {
-            int s = i;
-            int a = a1[i];
             int idx = l2;
             while (idx <= r2) {
-                a = a2[idx];
                 if(a1[i] == a2[idx]){
                     ++count;
                     a2[idx] = -1;
@@ -39,7 +35,39 @@ public class Arrays {
 
 	//EX3
 	public static void changeValueInMaxHeap(int[] v, int count, int ix, int newValue){
+		if(ix >= v.length) throw new IllegalArgumentException();
+		v[ix] = newValue;
+		buildMaxHeap(v, count);
+	}
 
+	public static void buildMaxHeap(int[] h, int n) {
+		for (int i = n / 2 - 1; i >= 0; i--)
+			maxHeapify(h, i, n);
+	}
+
+	public static int left(int i) { return 2 * i + 1; }
+
+	public static int right(int i) { return 2 * i + 2; }
+
+	public static void maxHeapify(int[] h, int i, int n) {
+		int l = left(i);
+		int r = right(i);
+		int largest;
+		if (l < n && h[l] > h[i])
+			largest = l;
+		else largest = i;
+		if (r < n && h[r] > h[largest])
+			largest = r;
+		if (largest != i) {
+			exchange(h, i, largest);
+			maxHeapify(h, largest, n);
+		}
+	}
+
+	private static void exchange(int[] h, int i, int j) {
+		int aux = h[i];
+		h[i] = h[j];
+		h[j] = aux;
 	}
 
 	//EX4
