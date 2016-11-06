0a42ebdb834baaae52fca8caf8b0a03327137d76
Pedro Santos <pedro.m.paixao.santos@gmail.com>
2016-11-06 20:56:30 +0000

    Begin Serie_2

--- src/Main.java
+++ src/Main.java
@@ -15,70 +15,10 @@ public class Main {
 	static String s3 = "f3.txt";
 
 	public static void main(String[] args){
-		int n = 25;
-		/*String s1 = "a", s2 = "b";
-		System.out.println(s1.compareTo(s2)); //-1
-		System.out.println(s2.compareTo(s1)); //1
-		System.out.println(s1.compareTo("")); //1
-		//System.out.println(s1.compareTo(null));
-		System.out.println(s1.equals(null));*/
-		GetFile getFile = new GetFile(n, s1, s2, s3);
-		/*String ip = "192.168.1.1";
-		String[] a = ip.split("\\.");*/
-		String[] ipv4 = new String[]{"192.168.1.1", "192.168.1.4", "80.68.11.31", "192.168.10.33"};
-		String[][] x = new String[ipv4.length][ipv4.length];
-		//array bi-dimensional
-		for(int i = 0; i< ipv4.length; ++i)
-			x[i] = ipv4[i].split("\\.");
+		/*******************************************/
+		/**              Serie_1                  **/
+		/********************************************/
+		//int n = 25;
+		//GetFile getFile = new GetFile(n, s1, s2, s3);
 	}
-
-	public static void ordena(int[] a, int m){
-		int n = a.length;
-
-		int vetorAuxiliar[] = new int[m];
-
-		//1ª - (Inicializar com zero)
-		for(int i = 0; i < m; i++){
-			vetorAuxiliar[i] = 0;
-		}
-
-		//2ª - Contagem das ocorrencias
-		for(int i = 0; i < n; i++){
-			vetorAuxiliar[a[i]]++;
-		}
-
-		//3ª - Ordenando os indices do vetor auxiliar
-		int sum = 0;
-		for(int i = 1; i < m; i++){
-			int dum = vetorAuxiliar[i];
-			vetorAuxiliar[i] = sum;
-			sum += dum;
-		}
-		int vetorOrdenado[] = new int[n];
-		for(int i = 0; i < n; i++){
-			vetorOrdenado[vetorAuxiliar[a[i]]] = a[i];
-			vetorAuxiliar[a[i]]++;
-		}
-
-		//4ª Retornando os valores ordenados para o vetor de entrada
-		for (int i = 0; i < n; i++){
-			a[i] = vetorOrdenado[i];
-		}
-	}
-
-
-
-	/*public static void sortIPv4Addresses(String[] v, int l, int r){
-		int idx, i = l+1;
-		int a1, a2;
-		String[] aux1 = new String[(r-l)+1];
-		String[] aux2 = new String[(r-l)+1];
-		while(l < r){
-			idx = 0;
-			aux1 = v[l].split("\\.");
-			aux2 = v[l+1].split("\\.");
-			a1 = Integer.parseInt(aux1[0]);
-			a2 = Integer.parseInt(aux2[0]);
-		}
-	}*/
 }
