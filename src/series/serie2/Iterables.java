1474fee90a1b911b7f739375078cce37f5d9d9fc
Snitram <diogomartins_151@hotmail.com>
2016-11-21 19:41:45 +0000

    2.1 quase feito, falta removeElements e Empty

--- src/series/serie2/Iterables.java
+++ src/series/serie2/Iterables.java
@@ -1,6 +1,9 @@
 package series.serie2;
 
 
+import java.util.*;
+import java.lang.*;
+
 public class Iterables {
 	
 	public static <T> Iterable<T> flatten( final Iterable< Iterable<T> > src) {
@@ -8,6 +11,22 @@ public class Iterables {
 	}
 
 	public static  Iterable<Integer> alternateEvenOdd(final Iterable<Integer> src){
-		throw new UnsupportedOperationException();
+		int count = 0;
+		Iterator<Integer> it = src.iterator();
+		List<Integer> aux = new LinkedList<>();
+		if(!it.hasNext()) {
+            throw NoSuchElementException;
+        }
+		while(it.hasNext()){
+			int number = it.next();
+			if(number%2==1 && count == 0) {
+				aux.add(number);
+				count = 1;
+			}else if(number%2==0 && count == 1){
+				aux.add(number);
+				count = 0;
+			}
+		}
+		return aux;
 	}
 }
