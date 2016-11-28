37edb25dbef5618adaabd2005e9f56cf11df0016
Pedro Santos <pedro.m.paixao.santos@gmail.com>
2016-11-28 22:35:12 +0000

    Begin Serie_2

--- src/series/serie2/Iterables.java
+++ src/series/serie2/Iterables.java
@@ -6,98 +6,95 @@ import java.util.*;
 
 public class Iterables {
 
-    public static <T> Iterable<T> flatten(final Iterable<Iterable<T>> src) {
-        return new Iterable<T>() {
-            Iterator<Iterable<T>> outIT = src.iterator();
-            Iterator<T> inIT = null;
-            T value = null;
-            @Override
-            public Iterator<T> iterator() {
-                return new Iterator<T>() {
-                    @Override
-                    public boolean hasNext() {
-                        if(value!=null)return true;
+	public static <T> Iterable<T> flatten(final Iterable<Iterable<T>> src) {
+		return new Iterable<T>() {
+			Iterator<Iterable<T>> outIT = src.iterator();
+			Iterator<T> inIT = null;
+			T value = null;
+			@Override
+			public Iterator<T> iterator() {
+				return new Iterator<T>() {
+					@Override
+					public boolean hasNext() {
+						if(value!=null)return true;
 
-                        if(inIT!=null){
-                            if(inIT.hasNext()) {
-                                value = inIT.next();
-                                return true;
-                            }
-                            inIT = null;
-                        }
+						if(inIT!=null){
+							if(inIT.hasNext()) {
+								value = inIT.next();
+								return true;
+							}
+							inIT = null;
+						}
 
-                        while(outIT.hasNext()){
-                            inIT = outIT.next().iterator();
-                            if(inIT.hasNext()) {
-                                value = inIT.next();
-                                return true;
-                            }
-                            inIT = null;
-                        }
-
-
-                        return false;
-                    }
-
-                    @Override
-                    public T next() {
-                        if(value==null) hasNext();
-                        T newVal=value;
-                        value = null;
-                        if(newVal==null) throw new NoSuchElementException("flatten: no more elements");
-                        return newVal;
-                    }
-                };
-            }
-        };
+						while(outIT.hasNext()){
+							inIT = outIT.next().iterator();
+							if(inIT.hasNext()) {
+								value = inIT.next();
+								return true;
+							}
+							inIT = null;
+						}
 
 
+						return false;
+					}
 
-    }
+					@Override
+					public T next() {
+						if(value==null) hasNext();
+						T newVal=value;
+						value = null;
+						if(newVal==null) throw new NoSuchElementException("flatten: no more elements");
+						return newVal;
+					}
+				};
+			}
+		};
 
-    public static Iterable<Integer> alternateEvenOdd(final Iterable<Integer> src) {
-        return new Iterable<Integer>() {
-            Iterator<Integer> it = src.iterator();
-            boolean isOdd = true;
-            int value = -1;
-            @Override
-            public Iterator<Integer> iterator() {
-                return new Iterator<Integer>() {
-                    @Override
-                    public boolean hasNext() {
-                        while(it.hasNext()){
-                            int number = it.next();
-                            if (number % 2 == 1 && isOdd) {
-                                value = number;
-                                isOdd = false;
-                                return true;
-                            } else if (number % 2 == 0 && !isOdd) {
-                                value = number;
-                                isOdd = true;
-                                return true;
-                            }
-                        }
-                        return false;
-                    }
 
-                    @Override
-                    public Integer next() {
-                        if(value==-1) hasNext();
-                        int newVal=value;
-                        value = -1;
-                        if(newVal==-1) throw new NoSuchElementException("alternateEvenoOdd: no more elements");
-                        return newVal;
-                    }
 
-                    @Override
-                    public void remove(){
-                        throw new UnsupportedOperationException("alternateEvenoOdd: remove not supported");
-                    }
-                };
-            }
-        };
-    }
+	}
+
+	public static Iterable<Integer> alternateEvenOdd(final Iterable<Integer> src) {
+		return new Iterable<Integer>() {
+			Iterator<Integer> it = src.iterator();
+			boolean isOdd = true;
+			int value = -1;
+			@Override
+			public Iterator<Integer> iterator() {
+				return new Iterator<Integer>() {
+					@Override
+					public boolean hasNext() {
+						while(it.hasNext()){
+							int number = it.next();
+							if (number % 2 == 1 && isOdd) {
+								value = number;
+								isOdd = false;
+								return true;
+							} else if (number % 2 == 0 && !isOdd) {
+								value = number;
+								isOdd = true;
+								return true;
+							}
+						}
+						return false;
+					}
+
+					@Override
+					public Integer next() {
+						if(value==-1) hasNext();
+						int newVal=value;
+						value = -1;
+						if(newVal==-1) throw new NoSuchElementException("alternateEvenoOdd: no more elements");
+						return newVal;
+					}
+
+					@Override
+					public void remove(){
+						throw new UnsupportedOperationException("alternateEvenoOdd: remove not supported");
+					}
+				};
+			}
+		};
+	}
 }
-
-
-
--- src/series/serie2/Utils.java
+++ src/series/serie2/Utils.java
@@ -1,10 +1,40 @@
 package series.serie2;
 
 public class Utils {
-	
-	public static <E> Node<E>[] shrink(Node<E>[] hashTable){
-		throw new UnsupportedOperationException();
+
+	public static <E> Node<E>[] shrink(Node<E>[] hashTable) {
+		Node<E>[] newTable = new Node[(hashTable.length)/2];
+		int newCode;
+		for(int i=0; i<hashTable.length; ++i){
+			if(hashTable[i]==hashTable[i].next)continue;
+			else {
+				newCode = newIndex(hashTable, i);
+				insert(hashTable,newCode,i);
+			}
+		}
+		for (int i = 0; i < newTable.length; i++) {
+			newTable[i]=hashTable[i];
+		}
+		hashTable=newTable;
+		return hashTable;
 	}
 
+	public static <E> int newIndex(Node<E>[] hashTable, int index) {
+		int newValue = hashTable.length / 2;
+		return hashTable[index].hashCode() % newValue;
+	}
 
+	public static <E> void insert(Node<E>[] hashtable,int targetIndex, int oldIndex){
+		if(hashtable[targetIndex]==hashtable[targetIndex].next){
+			hashtable[targetIndex].next = hashtable[oldIndex].next;
+			hashtable[oldIndex].next.previous = hashtable[targetIndex];
+			hashtable[targetIndex].previous = hashtable[oldIndex].previous;
+			hashtable[oldIndex].previous.next = hashtable[targetIndex];
+		}else{
+			hashtable[targetIndex].previous.next = hashtable[oldIndex].next;
+			hashtable[oldIndex].next.previous = hashtable[targetIndex].previous;
+			hashtable[oldIndex].previous.next = hashtable[targetIndex];
+			hashtable[targetIndex].previous = hashtable[oldIndex].previous;
+		}
+	}
 }
